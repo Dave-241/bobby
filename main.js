2d2db9bb52f89651a4fad7db8d48bf94fe447dbd
DAVIS ISIBOR <106489608+Dave-241@users.noreply.github.com>
2022-12-28 10:49:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function openNav() {
+  document.getElementById("sideNav").style.display = "flex";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.display = "none";
+}
+
+function prev_review() {
+  document.getElementById("comment").innerHTML = "consectetur nesciunt commodi maiores quidem eum est labore illo dolore quos autem harum cupiditaterepudiandae ipsa laudantium, loritaque nihil ex eaque possimus molestias libero? Qui odit beatae,ratione, quasi quia temporibus excepturi et deserunt sapiente blanditiis eligendi repellat. Loremipsum dolor sit amet consectetur adipisicin";
+
+  document.getElementById("name").innerHTML = "Mayorkon daniel";
+  document.getElementById("position").innerHTML = "assistant manager";
+  document.getElementById("rev1").style.transform = "scale(1.4)";
+  document.getElementById("rev2").style.transform = "scale(0.8)";
+  document.getElementById("rev3").style.transform = "scale(1)";
+
+}
+
+function next_review() {
+  document.getElementById("comment").innerHTML = "consectetur nesciunt commodi maiores quidem eum est labore illo dolore quos autem harum cupiditaterepudiandae ipsa laudantium, loritaque nihil ex eaque possimus molestias libero? Qui odit beatae,ratione, quasi quia temporibus excepturi et deserunt sapiente blanditiis eligendi repellat. Loremipsum dolor sit amet consectetur adipisicin, psa laudantium, loritaque nihil ex eaque possimus molestias libero? Qui odit beatae,ratione, quasi quia temporibus excepturi et deserunt sapiente blanditiis eligendi repellat. Loremipsum dolor sit amet consectetur adipisicin";
+
+  document.getElementById("name").innerHTML = "booby boss daniel";
+  document.getElementById("position").innerHTML = " manager";
+  document.getElementById("rev1").style.transform = "scale(1)";
+  document.getElementById("rev2").style.transform = "scale(0.8)";
+  document.getElementById("rev3").style.transform = "scale(1.4)";
+
+}
+
+function normal_review() {
+  document.getElementById("comment").innerHTML = "consectetur nesciunt commodi maiores quidem eum est labore illo doloreadipisicin, psa laudantium, loritaque nihil ex eaque possimus molestias libero? Qui odit beatae,ratione, quasi quia temporibus excepturi et deserunt sapiente blanditiis eligendi repellat. Loremipsum dolor sit amet consectetur adipisicin";
+
+  document.getElementById("name").innerHTML = "emma somandina";
+  document.getElementById("position").innerHTML = " programmer";
+  document.getElementById("rev1").style.transform = "scale(1)";
+  document.getElementById("rev2").style.transform = "scale(1)";
+  document.getElementById("rev3").style.transform = "scale(1)";
+
+}
+
