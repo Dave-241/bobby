c18e044277ee0ff60915082d2e26affe6a6d8ec9
DAVIS ISIBOR <davisisibor@gmail.com>
2022-12-30 20:19:23 +0100

    main

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,15 +3,24 @@ import { useState } from "react"
 const Nav = () => {
 
     const [hash, setHash] = useState("#")
+    const [Nav, setNav] = useState("none")
+
+    const openNav = () => {
+        setNav("flex")
+    }
+
+    const closeNav = () => {
+        setNav("none")
+    }
 
     return (
         <nav class="nav_wrap">
             <p class="logo">Company logo</p>
-            <button class="harmburger" onclick="openNav()"><i class="bi bi-list"></i></button>
+            <button class="harmburger" onClick={openNav}><i class="bi bi-list"></i></button>
 
-            <div class="mob_wrap" id="sideNav">
-                <div class="mob_nav " id="sideBar">
-                    <span class="close_nav" onclick="closeNav()">close</span>
+            <div class="mob_wrap" style={{ display: Nav }}>
+                <div class="mob_nav "  >
+                    <span class="close_nav" onClick={closeNav}>close</span>
                     <a href={hash} class="nav_list">About Us</a>
                     <a href={hash} class="nav_list">Projects</a>
                     <a href={hash} class="nav_list">Case studies</a>
